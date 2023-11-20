a2317d6816c701dc2a6ae8698a6a0099052c9dd4
Nichita S <nichita.saharov@plantonsolutions.com>
2023-11-20 20:36:08 +0200

    populate client data box with info

--- client/src/components/ClientPersonalDataBox.js
+++ client/src/components/ClientPersonalDataBox.js
@@ -2,89 +2,135 @@ import {Button, Form} from "react-bootstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCircleCheck} from "@fortawesome/free-regular-svg-icons";
 import {faPen} from "@fortawesome/free-solid-svg-icons";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {getClientByUsername} from "../api/clients";
 
 export default function ClientPersonalDataBox() {
     const [isDisabled, setIsDisabled] = useState(true);
     const handleEditClick = () => setIsDisabled(!isDisabled);
+    const [clientDetails, setClientDetails] = useState({
+        _id: '',
+        name: '',
+        surname: '',
+        tel: '',
+        email: '',
+        birthday: '',
+        nationality: '',
+        gender: '',
+        employer: '',
+        function: '',
+        avgSalary: 0,
+        passport: '',
+        profileStatus: '',
+        username: ''
+    });
+
+    useEffect(() => {
+        const fetchClientData = async () => {
+            try {
+                const response = await getClientByUsername('client');
+                setClientDetails(response.data)
+            } catch (error) {
+
+            }
+        }
+
+        fetchClientData();
+    })
+
+    const handleInputChange = (e) => {
+        const {name, value} = e.target;
+        setClientDetails({...clientDetails, [name]: value});
+    };
+
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+
+        reader.onload = () => {
+            const base64String = reader.result.split(',')[1];
+            setClientDetails({...clientDetails, passport: base64String});
+        };
+
+        reader.readAsDataURL(file);
+    };
 
     return (
         <>
             <Form>
-            <div className={'d-flex'}>
-                <Form.Group controlId="name" className={'client-data-form-group-l1'}>
-                    <Form.Label className={'ms-1'}>Nume</Form.Label>
-                    <Form.Control
-                        type={'text'}
-                        // placeholder="Introduceti modelul"
-                        name="firstName"
-                        // value={farmEquipmentDetails.model}
-                        // onChange={handleInputChange}
-                        className={'client-data-name-control'}
-                        required
-                    />
-                </Form.Group>
-                <Form.Group controlId="name" className={'client-data-form-group-l1'}>
-                    <Form.Label className={'ms-1'}>Prenume</Form.Label>
-                    <Form.Control
-                        type={'text'}
-                        // placeholder="Introduceti modelul"
-                        name="firstName"
-                        // value={farmEquipmentDetails.model}
-                        // onChange={handleInputChange}
-                        className={'client-data-name-control'}
-                        required
-                    />
-                </Form.Group>
-            </div>
                 <div className={'d-flex'}>
-                    <Form.Group controlId="name" className={'client-data-form-group-f'}>
-                        <Form.Label className={'ms-1'}>IDNP</Form.Label>
+                    <Form.Group className={'client-data-form-group-l1'}>
+                        <Form.Label className={'ms-1'}>Nume</Form.Label>
                         <Form.Control
                             type={'text'}
-                            // placeholder="Introduceti modelul"
-                            name="firstName"
-                            // value={farmEquipmentDetails.model}
-                            // onChange={handleInputChange}
-                            className={'client-data-contact-control'}
+                            placeholder={clientDetails.name}
+                            name="name"
+                            onChange={handleInputChange}
+                            className={'client-data-name-control'}
+                            disabled={isDisabled}
                             required
                         />
                     </Form.Group>
-                    <Form.Group controlId="name" className={'client-data-form-group'}>
-                        <Form.Label className={'ms-1'}>Telefon mobil</Form.Label>
+                    <Form.Group className={'client-data-form-group-l1'}>
+                        <Form.Label className={'ms-1'}>Prenume</Form.Label>
                         <Form.Control
                             type={'text'}
-                            // placeholder="Introduceti modelul"
-                            name="firstName"
-                            // value={farmEquipmentDetails.model}
-                            // onChange={handleInputChange}
-                            className={'client-data-contact-control'}
-                            required
-                        />
-                    </Form.Group>
-                    <Form.Group controlId="name" className={'client-data-form-group'}>
-                        <Form.Label className={'ms-1'}>Email</Form.Label>
-                        <Form.Control
-                            type={'text'}
-                            // placeholder="Introduceti modelul"
-                            name="firstName"
-                            // value={farmEquipmentDetails.model}
-                            // onChange={handleInputChange}
-                            className={'client-data-contact-control'}
+                            placeholder={clientDetails.surname}
+                            name="surname"
+                            onChange={handleInputChange}
+                            className={'client-data-name-control'}
+                            disabled={isDisabled}
                             required
                         />
                     </Form.Group>
                 </div>
                 <div className={'d-flex'}>
-                    <Form.Group controlId="name" className={'client-data-form-group-f'}>
+                    <Form.Group className={'client-data-form-group-f'}>
+                        <Form.Label className={'ms-1'}>IDNP</Form.Label>
+                        <Form.Control
+                            type={'text'}
+                            placeholder={clientDetails._id}
+                            name="idnp"
+                            onChange={handleInputChange}
+                            className={'client-data-contact-control'}
+                            disabled={isDisabled}
+                            required
+                        />
+                    </Form.Group>
+                    <Form.Group className={'client-data-form-group'}>
+                        <Form.Label className={'ms-1'}>Telefon mobil</Form.Label>
+                        <Form.Control
+                            type={'text'}
+                            placeholder={clientDetails.tel}
+                            name="tel"
+                            onChange={handleInputChange}
+                            className={'client-data-contact-control'}
+                            disabled={isDisabled}
+                            required
+                        />
+                    </Form.Group>
+                    <Form.Group className={'client-data-form-group'}>
+                        <Form.Label className={'ms-1'}>Email</Form.Label>
+                        <Form.Control
+                            type={'text'}
+                            placeholder={clientDetails.email}
+                            name="firstName"
+                            onChange={handleInputChange}
+                            className={'client-data-contact-control'}
+                            disabled={isDisabled}
+                            required
+                        />
+                    </Form.Group>
+                </div>
+                <div className={'d-flex'}>
+                    <Form.Group className={'client-data-form-group-f'}>
                         <Form.Label className={'ms-1'}>Data nasterii</Form.Label>
                         <Form.Control
                             type={'text'}
-                            // placeholder="Introduceti modelul"
-                            name="firstName"
-                            // value={farmEquipmentDetails.model}
-                            // onChange={handleInputChange}
+                            placeholder={clientDetails.birthday}
+                            name="birthday"
                             className={'client-data-pers-info-control'}
+                            disabled={true}
                             required
                         />
                     </Form.Group>
@@ -92,101 +138,60 @@ export default function ClientPersonalDataBox() {
                         <Form.Label className={'ms-1'}>Nationalitate</Form.Label>
                         <Form.Control
                             type={'text'}
-                            // placeholder="Introduceti modelul"
-                            name="firstName"
-                            // value={farmEquipmentDetails.model}
-                            // onChange={handleInputChange}
+                            placeholder={clientDetails.nationality}
+                            name="nationality"
                             className={'client-data-pers-info-control'}
+                            disabled={true}
                             required
                         />
                     </Form.Group>
-                    <Form.Group controlId="name" className={'client-data-form-group'}>
+                    <Form.Group className={'client-data-form-group'}>
                         <Form.Label className={'ms-1'}>Sex</Form.Label>
                         <Form.Control
                             type={'text'}
-                            // placeholder="Introduceti modelul"
-                            name="firstName"
-                            // value={farmEquipmentDetails.model}
-                            // onChange={handleInputChange}
+                            placeholder={clientDetails.gender}
+                            name="gender"
                             className={'client-data-pers-info-control'}
+                            disabled={true}
                             required
                         />
                     </Form.Group>
                 </div>
-                {/*<div className={'d-flex'}>*/}
-                {/*    <Form.Group controlId="name" className={'client-data-form-group-f'}>*/}
-                {/*        <Form.Label className={'ms-1'}>ID</Form.Label>*/}
-                {/*        <Form.Control*/}
-                {/*            type={'text'}*/}
-                {/*            // placeholder="Introduceti modelul"*/}
-                {/*            name="firstName"*/}
-                {/*            // value={farmEquipmentDetails.model}*/}
-                {/*            // onChange={handleInputChange}*/}
-                {/*            className={'client-data-id-control'}*/}
-                {/*            required*/}
-                {/*        />*/}
-                {/*    </Form.Group>*/}
-                {/*    <Form.Group controlId="name" className={'client-data-form-group'}>*/}
-                {/*        <Form.Label className={'ms-1'}>IDNP</Form.Label>*/}
-                {/*        <Form.Control*/}
-                {/*            type={'text'}*/}
-                {/*            // placeholder="Introduceti modelul"*/}
-                {/*            name="firstName"*/}
-                {/*            // value={farmEquipmentDetails.model}*/}
-                {/*            // onChange={handleInputChange}*/}
-                {/*            className={'client-data-id-control'}*/}
-                {/*            required*/}
-                {/*        />*/}
-                {/*    </Form.Group>*/}
-                {/*    <Form.Group controlId="name" className={'client-data-form-group'}>*/}
-                {/*        <Form.Label className={'ms-1'}>№ Permisului de conducere</Form.Label>*/}
-                {/*        <Form.Control*/}
-                {/*            type={'text'}*/}
-                {/*            // placeholder="Introduceti modelul"*/}
-                {/*            name="firstName"*/}
-                {/*            // value={farmEquipmentDetails.model}*/}
-                {/*            // onChange={handleInputChange}*/}
-                {/*            className={'client-data-id-control'}*/}
-                {/*            required*/}
-                {/*        />*/}
-                {/*    </Form.Group>*/}
-                {/*</div>*/}
                 <div className={'d-flex'}>
-                    <Form.Group controlId="name" className={'client-data-form-group-f'}>
+                    <Form.Group className={'client-data-form-group-f'}>
                         <Form.Label className={'ms-1'}>Numele angajatorului</Form.Label>
                         <Form.Control
                             type={'text'}
-                            // placeholder="Introduceti modelul"
-                            name="firstName"
-                            // value={farmEquipmentDetails.model}
-                            // onChange={handleInputChange}
+                            placeholder={clientDetails.employer}
+                            name="employer"
+                            onChange={handleInputChange}
                             className={'client-data-emp-control'}
+                            disabled={isDisabled}
                             required
                         />
                     </Form.Group>
-                    <Form.Group controlId="name" className={'client-data-form-group'}>
+                    <Form.Group className={'client-data-form-group'}>
                         <Form.Label className={'ms-1'}>Functie</Form.Label>
                         <Form.Control
                             type={'text'}
-                            // placeholder="Introduceti modelul"
-                            name="firstName"
-                            // value={farmEquipmentDetails.model}
-                            // onChange={handleInputChange}
+                            placeholder={clientDetails.function}
+                            name="function"
+                            onChange={handleInputChange}
                             className={'client-data-emp-control'}
+                            disabled={isDisabled}
                             required
                         />
                     </Form.Group>
-                    <Form.Group controlId="name" className={'client-data-form-group'}>
+                    <Form.Group className={'client-data-form-group'}>
                         <Form.Label className={'ms-1'}>Venit mediu</Form.Label>
                         <Form.Control
-                            type={'text'}
-                            // placeholder="Introduceti modelul"
-                            name="firstName"
-                            // value={farmEquipmentDetails.model}
-                            // onChange={handleInputChange}
+                            type={'number'}
+                            placeholder={clientDetails.avgSalary.toLocaleString('en-US')}
+                            name="avgSalary"
+                            onChange={handleInputChange}
                             className={'client-data-emp-control'}
-                            required
                             disabled={isDisabled}
+                            required
                         />
                     </Form.Group>
                 </div>
@@ -196,15 +201,17 @@ export default function ClientPersonalDataBox() {
                         <Form.Control
                             type="file"
                             accept="image/*"
-                            // onChange={handleImageChange}
+                            onChange={handleImageChange}
                             className={'client-pass-pic'}
-                            style={{width:'90%'}}
+                            style={{width: '90%'}}
+                            disabled={isDisabled}
                         />
                     </Form.Group>
                 </div>
                 <div className={'d-flex justify-content-end mt-4 me-4'}>
-                    <Button variant={"light"} className={'border-0 bg-transparent me-4 client-btn'} onClick={handleEditClick}>
-                        <FontAwesomeIcon icon={faPen} />
+                    <Button variant={"light"} className={'border-0 bg-transparent me-4 client-btn'}
+                            onClick={handleEditClick}>
+                        <FontAwesomeIcon icon={faPen}/>
                     </Button>
                     <Button variant={"light"} className={'border-0 bg-transparent client-btn'}>
                         <FontAwesomeIcon fontVariant={'solid'} icon={faCircleCheck} className={'me-2'}/>
--- client/src/pages/ClientDashboard.js
+++ client/src/pages/ClientDashboard.js
@@ -3,14 +3,25 @@ import NavigationBar from "../components/NavigationBar";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCircleNodes, faRightFromBracket, faUserTie} from "@fortawesome/free-solid-svg-icons";
 import {Button} from "react-bootstrap";
-import React from "react";
+import React, {useState} from "react";
 import {signOut} from "../api/user";
 import {RoutesEnum} from "../components/RoutesEnum";
 import ClientPersonalDataBox from "../components/ClientPersonalDataBox";
 
 export default function ClientDashboard() {
-    // const location = useLocation();
     const navigate = useNavigate();
+    const Buttons = {
+        clientData: 'clientData',
+        clientContracts: 'clientContracts'
+    }
+    const [renderedCmp, setRenderedCmp] = useState(Buttons.clientData);
+
+    const handleInfoBtnClick = () => {
+        setRenderedCmp(Buttons.clientData)
+    }
+    const handleContractsBtnClick = () => {
+        setRenderedCmp(Buttons.clientContracts)
+    }
 
     const handleSignOutClick = async () => {
         try {
@@ -26,12 +37,14 @@ export default function ClientDashboard() {
             <NavigationBar/>
             <div className={'d-flex justify-content-around mt-3'}>
                 <span>
-                    <Button className={'bg-transparent border-0'} variant="light" size="lg">
+                    <Button className={'bg-transparent border-0'} variant="light" size="lg"
+                            onClick={handleInfoBtnClick}>
                        <FontAwesomeIcon icon={faUserTie} className={'client-btn'}/>
                     </Button>
                 </span>
                 <span>
-                    <Button className={'bg-transparent border-0'} variant="light" size="lg">
+                    <Button className={'bg-transparent border-0'} variant="light" size="lg"
+                            onClick={handleContractsBtnClick}>
                         <FontAwesomeIcon icon={faCircleNodes} className={'client-btn'}/>
                     </Button>
                 </span>
@@ -43,9 +56,11 @@ export default function ClientDashboard() {
                 </span>
             </div>
             <div className={'d-flex justify-content-center'}>
-                <div className={'mt-3 client-data-box'}>
-                    <ClientPersonalDataBox/>
-                </div>
+                {renderedCmp === Buttons.clientData &&
+                    <div className={'mt-3 client-data-box'}>
+                        <ClientPersonalDataBox/>
+                    </div>
+                }
             </div>
         </>
     );
